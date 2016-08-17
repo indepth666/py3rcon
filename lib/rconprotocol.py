b759776659b40751db5e9f5ae331efbd51bbc1b0
ole1986 <ole.k@web.de>
2016-08-17 18:36:52 +0200

    code cleanup and comments

--- lib/rconprotocol.py
+++ lib/rconprotocol.py
@@ -221,8 +221,6 @@ class Rcon():
         p = packet[0]
         try:
             if p[0:2] == b'BE':
-
-                #print(p[8:9])
                 self.s.sendto(self._acknowledge(p[8:9]), (self.ip, self.port))
         except:
             pass
@@ -257,7 +255,6 @@ class Rcon():
             logging.info("[Server: %s:%s]: %s" % (self.ip, self.port, stream))
             logging.debug("[Server: %s:%s]: %s" % (self.ip, self.port, packet))
 
-
     def __players(self, pl):
         l = []
         for m in pl:
@@ -289,7 +286,6 @@ class Rcon():
                 if m:
                     action(m.group())
                 break
-
 
     """
     public: send a chat message to everyone
