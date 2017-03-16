00ddee344d70063a740d679782304b9f277dc93a
ole1986 <ole.k@web.de>
2017-03-17 00:14:45 +0100

    message box implemented

--- web/js/py3rcon.js
+++ web/js/py3rcon.js
@@ -1,16 +1,48 @@
-new Vue({
-    el: '#player',
+var AlertVue = new Vue({
+    el: '#alert',
     data: {
-        players: []
-    },
-
-    mounted: function(){
-        var vm = this;
-        $.getJSON('/action/getplayers', function (data) {
-            vm.players = data;
-        });
+        message: '',
+        type: '',
+        visible: false,
+        allowClose: false
     },
     methods: {
+        Show: function(msg, t, closeable){
+            this.message = msg;
+            this.type = t;
+            this.visible = true;
+            this.allowClose = closeable;
+        },
+        Hide: function(){
+            this.message = '';
+            this.visible = false;
+        }
+    }
+});
+
+var playerVue = new Vue({
+    el: '#player',
+    data: {
+        players: [],
+    },
+    mounted: function(){
+        this.Load();
+    },
+    methods: {
+        Load: function(){
+            var vm = this;
+            AlertVue.Show('Loading...', 'alert-info');
+            $.getJSON('/action/getplayers', function (data) {
+                vm.players = data;
+                AlertVue.Hide();
+            });
+        },
+        Restart: function(){
+            AlertVue.Show('Not yet implemented', 'alert-info', true);
+        },
+        Shutdown: function(){
+            AlertVue.Show('Not yet implemented', 'alert-info', true);
+        },
         Kick: function(item){
             var vm = this;
             console.log(item);
